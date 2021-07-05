cb1789803f1d7eadc5e224e068a6267b2ea03d67
alvaromunoz <alvaromsg@gmail.com>
2021-07-05 23:07:29 +0000

    Some modifictions

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,23 +1,11 @@
-variable "location" {
-  type = string
-  description = "Región de Azure donde crearemos la infraestructura"
-  default = "West Europe"
-}
+#variable "location" {
+#  type = string
+#  description = "Región de Azure donde crearemos la infraestructura"
+#  default = "West Europe"
+#}
 
-variable "storage_account" {
+variable "vm_size" {
   type = string
-  description = "Nombre para la storage account"
-  default = "storageaccountalvaromsgcp2"
-}
-
-variable "public_key_path" {
-  type = string
-  description = "Ruta para la clave pública de acceso a las instancias"
-  default = "~/.ssh/id_rsa.pub" # o la ruta correspondiente
-}
-
-variable "ssh_user" {
-  type = string
-  description = "Usuario para hacer ssh"
-  default = "adminUsername"
+  description = "Tamaño de la máquina virtual"
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
